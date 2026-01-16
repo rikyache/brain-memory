097548abfbfbddde522fcc2c2d9767a9e93d7454
sxd0 <fancityfun@gmail.com>
2026-01-16 19:21:49 +0500

    add pic

--- app.config.js
+++ app.config.js
@@ -1,7 +1,13 @@
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-  },
-});
+export default ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      ...config.extra,
+      eas: {
+        projectId: "960bc415-9c26-4d1d-b7f6-3333291e39bd"
+      },
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    },
+  };
+};
